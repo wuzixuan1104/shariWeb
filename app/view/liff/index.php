c69e0cbdc12ffd95efb9286572fc8791a461ae26
wu-tzu-hsuan <cherry51120@gmail.com>
2019-03-19 14:13:49 +0800

    f

--- app/view/liff/index.php
+++ app/view/liff/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta http-equiv="Content-Language" content="zh-tw">
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
 
     <title>請填寫表單</title>
 
