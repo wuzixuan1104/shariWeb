c2fa3a8f5d51ac413eb56e72ee4f94ce089e3701
wu-tzu-hsuan <cherry51120@gmail.com>
2019-04-16 12:41:50 +0800

    fix

--- app/view/liff/index.php
+++ app/view/liff/index.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
-<html lang="zh-Hant">
+
   <head>
     <meta http-equiv="Content-Language" content="zh-tw">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- <meta name="viewport" content="width=device-width,initial-scale=1"> -->
 
     <title>信件內容</title>
 
@@ -53,4 +53,4 @@
       }
     </script> -->
   </body>
-</html>
+
