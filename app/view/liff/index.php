21965b96db3e1c346ff039e184d8c32d390b530f
wu-tzu-hsuan <cherry51120@gmail.com>
2019-04-16 11:41:13 +0800

    fix

--- app/view/liff/index.php
+++ app/view/liff/index.php
@@ -13,16 +13,22 @@
   </head>
   <body>
     <form class="login_form">
-      <div class="list">
-        <label class="row">
-          <textarea id="myTextarea" name="content" required placeholder="請輸入內容"></textarea>
-        </label>
-        <label class="row">
-          <input id="getProfile" type="button" value="取得使用者資料">
-          <input id="send" type="submit" value="送出">
-        </label>
+      <div class="row">
+        <div id="pic"><img src="/asset/img/me.png"></div>
+        <!-- <div id="pic"></div> -->
+        <span class="name" id="name">Shari</span>
+      </div>
+
+      <div class="row">
+        <textarea id="myTextarea" name="content" required placeholder="請輸入內容"></textarea>
+      </div>
+
+      <div class="row">
+        <!-- <input id="getProfile" type="button" value="取得使用者資料"> -->
+        <input id="send" type="submit" value="送出">
       </div>
     </form>
+
     <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
     <script>
       window.onload = function (e) {
@@ -33,19 +39,34 @@
 
       function initializeApp(data) {
         document.getElementById('send').addEventListener('click', function () {
+            if (document.getElementById('myTextarea').value == '')
+              return false;
+
             liff.closeWindow();
         });
 
-        document.getElementById('getProfile').addEventListener('click', function () {
+        // document.getElementById('getProfile').addEventListener('click', function () {
             liff.getProfile().then(function (profile) {
-                var html = 'user_id = ' + profile.userId + '<br>';
-                html += 'display_name = ' + profile.displayName;
+                // var html = 'user_id = ' + profile.userId + '<br>';
+                // html += 'display_name = ' + profile.displayName;
+
                 document.getElementById('myTextarea').value = html;
-                
+                document.getElementById('name').html = profile.displayName;
+
+                const pictureDiv = document.getElementById('pic');
+                const img = document.createElement('img');
+                img.src = profile.pictureUrl;
+                img.alt = "Profile Picture";
+                pictureDiv.appendChild(img);
+
             }).catch(function (error) {
                 window.alert("Error getting profile: " + error);
             });
-        });
+
+
+        // });
+
+        
       }
 
 
