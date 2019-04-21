be8a242f8b3e17de5070c21960c9be2188b16065
吳姿萱 <cherry51120@gmail.com>
2019-04-21 13:59:55 +0800

    test

--- app/view/site/Main/index.php
+++ app/view/site/Main/index.php
@@ -6,10 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
 
     <title>Maple</title>
- 
-  </head>
-  <body lang="zh-tw">
-    <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase.js"></script>
     <script>
       // Initialize Firebase
       var config = {
@@ -21,22 +17,71 @@
         messagingSenderId: "612781458571"
       };
       firebase.initializeApp(config);
+    </script>
 
+  </head>
+  <body lang="zh-tw">
+    <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase.js"></script>
+    <script>
       const messaging = firebase.messaging();
 
-      messaging.requestPermission()
-        .then(res => {
-          // 若允許通知 -> 向 firebase 拿 token
-          return messaging.getToken();
-        }, err => {
-          // 若拒絕通知
-          console.log(err);  
+      messaging.onTokenRefresh(function() {
+       console.log("onTokenRefresh");
+       try_to_get_token();
+      });
+
+      messaging.onMessage(function(payload) {
+       console.log("Message received. ", payload);
+      });
+
+
+      if ('serviceWorker' in navigator) {
+       navigator.serviceWorker.register('service_worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        messaging.useServiceWorker(registration);
+        //try_to_get_token();
+        messaging.requestPermission()
+         .then(function() {
+          console.log('Notification permission granted.');
+          try_to_get_token();
+         })
+         .catch(function(err) {
+          console.log('Unable to get permission to notify.', err);
+         });
+      
+       }).catch(function(err) {
+        //registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+       });
+      }
+
+      function try_to_get_token() {
+       messaging.getToken()
+        .then(function(currentToken) {
+         if (currentToken) {
+          console.log("currentToken:", currentToken);
+         } else {
+          console.log('No Instance ID token available. Request permission to generate one.');
+         }
         })
-        .then(token => {
-          // 成功取得 token
-          // postToken(token); // 打給後端 api
-          console.log(token);
+        .catch(function(err) {
+         console.log('An error occurred while retrieving token. ', err);
         });
+      }
+  
+      // messaging.requestPermission()
+      //   .then(res => {
+      //     // 若允許通知 -> 向 firebase 拿 token
+      //     return messaging.getToken();
+      //   }, err => {
+      //     // 若拒絕通知
+      //     console.log(err);  
+      //   })
+      //   .then(token => {
+      //     // 成功取得 token
+      //     // postToken(token); // 打給後端 api
+      //     console.log(token);
+      //   });
     </script>
 
   </body>
