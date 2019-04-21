81b57ada44a7bfa328f9dd0803fcec738a94af00
吳姿萱 <cherry51120@gmail.com>
2019-04-21 23:36:12 +0800

    t

--- app/view/site/Main/index.php
+++ app/view/site/Main/index.php
@@ -35,77 +35,10 @@
         console.log('Error occurred!');
       });
 
-
       messaging.onMessage(function(payload) {
         console.log('onMessage:', payload);
       });
 
-
-
-
-
-
-
-
-
-      // messaging.onTokenRefresh(function() {
-      //  console.log("onTokenRefresh");
-      //  try_to_get_token();
-      // });
-
-      // messaging.onMessage(function(payload) {
-      //  console.log("Message received. ", payload);
-      // });
-
-
-      // if ('serviceWorker' in navigator) {
-      //  navigator.serviceWorker.register('service_worker.js').then(function(registration) {
-      //   console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      //   messaging.useServiceWorker(registration);
-      //   //try_to_get_token();
-      //   messaging.requestPermission()
-      //    .then(function() {
-      //     console.log('Notification permission granted.');
-      //     try_to_get_token();
-      //    })
-      //    .catch(function(err) {
-      //     console.log('Unable to get permission to notify.', err);
-      //    });
-      
-      //  }).catch(function(err) {
-      //   //registration failed :(
-      //   console.log('ServiceWorker registration failed: ', err);
-      //  });
-      // }
-
-      // function try_to_get_token() {
-      //  messaging.getToken()
-      //   .then(function(currentToken) {
-      //    if (currentToken) {
-      //     console.log("currentToken:", currentToken);
-      //    } else {
-      //     console.log('No Instance ID token available. Request permission to generate one.');
-      //    }
-      //   })
-      //   .catch(function(err) {
-      //    console.log('An error occurred while retrieving token. ', err);
-      //   });
-      // }
-
-
-      // messaging.requestPermission()
-      //   .then(res => {
-      //     // 若允許通知 -> 向 firebase 拿 token
-      //     return messaging.getToken();
-      //   }, err => {
-      //     // 若拒絕通知
-      //     console.log(err);  
-      //   })
-      //   .then(token => {
-      //     // 成功取得 token
-      //     // postToken(token); // 打給後端 api
-      //     console.log(token);
-      //   });
     </script>
 
   </body>
