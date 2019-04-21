0af5845bfb327b67b74ce2fe9af5e84adc243144
吳姿萱 <cherry51120@gmail.com>
2019-04-21 13:42:33 +0800

    test firebase

--- app/view/site/Main/index.php
+++ app/view/site/Main/index.php
@@ -9,5 +9,35 @@
  
   </head>
   <body lang="zh-tw">
+    <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase.js"></script>
+    <script>
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyAKJ4shHKjGJJKSAYyeNLJLdy2UhxoHM4g",
+        authDomain: "tripsaas-test.firebaseapp.com",
+        databaseURL: "https://tripsaas-test.firebaseio.com",
+        projectId: "tripsaas-test",
+        storageBucket: "tripsaas-test.appspot.com",
+        messagingSenderId: "612781458571"
+      };
+      firebase.initializeApp(config);
+
+      const messaging = firebase.messaging();
+
+      messaging.requestPermission()
+        .then(res => {
+          // 若允許通知 -> 向 firebase 拿 token
+          return messaging.getToken();
+        }, err => {
+          // 若拒絕通知
+          console.log(err);  
+        })
+        .then(token => {
+          // 成功取得 token
+          // postToken(token); // 打給後端 api
+          console.log(token);
+        });
+    </script>
+
   </body>
 </html>
