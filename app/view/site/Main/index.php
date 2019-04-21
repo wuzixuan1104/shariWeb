c7113b290bfc7afd2d75b8b71acef8d7eda63a46
吳姿萱 <cherry51120@gmail.com>
2019-04-21 14:52:27 +0800

    test

--- app/view/site/Main/index.php
+++ app/view/site/Main/index.php
@@ -23,50 +23,75 @@
   <body lang="zh-tw">
     <script>
       const messaging = firebase.messaging();
-
-      messaging.onTokenRefresh(function() {
-       console.log("onTokenRefresh");
-       try_to_get_token();
+      
+      messaging.requestPermission().then(function() {
+        console.log('Have permission');
+        return messaging.getToken();
+      })
+      .then(function(token) {
+        console.log(token);
+      })
+      .catch(function(err) {
+        console.log('Error occurred!');
       });
+
 
       messaging.onMessage(function(payload) {
-       console.log("Message received. ", payload);
+        console.log('onMessage:', payload);
       });
 
 
-      if ('serviceWorker' in navigator) {
-       navigator.serviceWorker.register('service_worker.js').then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        messaging.useServiceWorker(registration);
-        //try_to_get_token();
-        messaging.requestPermission()
-         .then(function() {
-          console.log('Notification permission granted.');
-          try_to_get_token();
-         })
-         .catch(function(err) {
-          console.log('Unable to get permission to notify.', err);
-         });
-      
-       }).catch(function(err) {
-        //registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-       });
-      }
 
-      function try_to_get_token() {
-       messaging.getToken()
-        .then(function(currentToken) {
-         if (currentToken) {
-          console.log("currentToken:", currentToken);
-         } else {
-          console.log('No Instance ID token available. Request permission to generate one.');
-         }
-        })
-        .catch(function(err) {
-         console.log('An error occurred while retrieving token. ', err);
-        });
-      }
+
+
+
+
+
+
+      // messaging.onTokenRefresh(function() {
+      //  console.log("onTokenRefresh");
+      //  try_to_get_token();
+      // });
+
+      // messaging.onMessage(function(payload) {
+      //  console.log("Message received. ", payload);
+      // });
+
+
+      // if ('serviceWorker' in navigator) {
+      //  navigator.serviceWorker.register('service_worker.js').then(function(registration) {
+      //   console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      //   messaging.useServiceWorker(registration);
+      //   //try_to_get_token();
+      //   messaging.requestPermission()
+      //    .then(function() {
+      //     console.log('Notification permission granted.');
+      //     try_to_get_token();
+      //    })
+      //    .catch(function(err) {
+      //     console.log('Unable to get permission to notify.', err);
+      //    });
+      
+      //  }).catch(function(err) {
+      //   //registration failed :(
+      //   console.log('ServiceWorker registration failed: ', err);
+      //  });
+      // }
+
+      // function try_to_get_token() {
+      //  messaging.getToken()
+      //   .then(function(currentToken) {
+      //    if (currentToken) {
+      //     console.log("currentToken:", currentToken);
+      //    } else {
+      //     console.log('No Instance ID token available. Request permission to generate one.');
+      //    }
+      //   })
+      //   .catch(function(err) {
+      //    console.log('An error occurred while retrieving token. ', err);
+      //   });
+      // }
+
 
       // messaging.requestPermission()
       //   .then(res => {
