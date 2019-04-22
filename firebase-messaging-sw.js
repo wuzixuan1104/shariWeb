3900d4a93cf8cc2ee5933eeec9383e5d61e991f9
吳姿萱 <cherry51120@gmail.com>
2019-04-22 23:47:27 +0800

    t

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -22,5 +22,19 @@ messaging.setBackgroundMessageHandler(function(payload) {
     body: payload.data.status
   };
 
+  if (!("Notification" in window)) {
+    console.log("This browser does not support system notifications");
+  }
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification(notificationTitle,notificationOptions);
+      notification.onclick = function(event) {
+          event.preventDefault(); // prevent the browser from focusing the Notification's tab
+          window.open(payload.data.click_action , '_blank');
+          notification.close();
+      }
+  }
+
   return self.registration.showNotification(title, options);
 });
