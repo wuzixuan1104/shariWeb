1815002e6f2e936fb9aeca689efe2689b71358e2
wu-tzu-hsuan <cherry51120@gmail.com>
2019-04-23 14:58:19 +0800

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -22,19 +22,12 @@ messaging.setBackgroundMessageHandler(function(payload) {
     body: payload.data.body
   };
 
-  // if (!("Notification" in window)) {
-  //   console.log("This browser does not support system notifications");
-  // }
-  // // Let's check whether notification permissions have already been granted
-  // else if (Notification.permission === "granted") {
-  //     // If it's okay let's create a notification
-  //     var notification = new Notification(notificationTitle,notificationOptions);
-  //     notification.onclick = function(event) {
-  //         event.preventDefault(); // prevent the browser from focusing the Notification's tab
-  //         window.open(payload.data.click_action , '_blank');
-  //         notification.close();
-  //     }
-  // }
+  self.addEventListener('notificationclick', function(event) {
+    console.log('background click');
+
+    // Do something as the result of the notification click
+  });
+  
 
   return self.registration.showNotification(title, options);
 });
