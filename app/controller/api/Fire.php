ff17fe4a9ff883aec041ba41a25adaea509e8f77
wu-tzu-hsuan <cherry51120@gmail.com>
2019-05-03 11:56:41 +0800

    t

--- app/controller/api/Fire.php
+++ app/controller/api/Fire.php
@@ -14,50 +14,31 @@ class Fire extends ApiController {
 
     $messaging = $firebase->getMessaging();
 
-    $posts = Input::post();
-  
-    // 用 token 取得 cs_chat_room
-    // $room = $this->chat_model->getRoom($msg['type'], $msg['token']);
-    // 更新 room.unread_cnt++ 
-
-    // 用 profile_id 取得所有 cs_chat_assign
-    
-    $assigns = [
-        [
-            'ae_sn' => '',
-            'notify_token' => 'fJrRjIzAiGs:APA91bErclLyTMBw_ZqykvVpVAsB11LfqYydpQyQ9Ynr1VkDfhGfrpE7NjxL6WfXEDKb1WT7sRNio1cTZvIjIqHukvDH3tz8SWUl8GN8fmd67Ikx3i1TqcYWVzBH40RD3IezzoyN6Xj5',
-            'unread_cnt' => 0,
-        ]
-    ];
-
-    foreach ($assigns as $assign) {
-        // assign.unread_cnt++
-        // assign.notify 推播（目前都要推播）
-        $message = CloudMessage::fromArray([
-            'token' => $assign['notify_token'],
-            'data' => [
-                'title' => 'Shari 傳送訊息',
-                'body' => $posts['text'],
-                'icon' => '/asset/img/me.png',
-                'click_action' => 'https://trip.web.shari.tw/chat/2',
-            ],
+    $db = $firebase->getDatabase();
+    $db->getReference('cs/1')
+       ->set([
+            'date' => date('Y-m-d H:i:s', strtotime('+3 min')),
         ]);
 
-        $messaging->send($message);
-        return true;
-        
-    }
-        
-    // 取得客服 token 
-           
-    // 新增 cs_chat_dialog
-    // $params = [
-    //     'cs_chat_room_id' => $room['id'],
-    //     'ae_sn' => $assign['ae_sn'],
-    //     'us_profile_id' => $room['us_profile_id'],
-    //     'content' => $msgs['text'],
-    // ]
-
+    $posts = Input::post();
+    
+    $assign = [
+        'ae_sn' => '',
+        'notify_token' => 'fJrRjIzAiGs:APA91bErclLyTMBw_ZqykvVpVAsB11LfqYydpQyQ9Ynr1VkDfhGfrpE7NjxL6WfXEDKb1WT7sRNio1cTZvIjIqHukvDH3tz8SWUl8GN8fmd67Ikx3i1TqcYWVzBH40RD3IezzoyN6Xj5',
+        'unread_cnt' => 0,
+    ];
+    
+    $messaging->send(CloudMessage::fromArray([
+        'token' => $assign['notify_token'],
+        'data' => [
+            'title' => 'Shari 傳送訊息',
+            'body' => $posts['text'],
+            'icon' => '/asset/img/me.png',
+            'click_action' => 'https://trip.web.shari.tw/chat/2',
+        ],
+    ]));
+    
+    return true;
   }
   
 }
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,59 +1,70 @@
 importScripts('https://www.gstatic.com/firebasejs/5.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.10.0/firebase-messaging.js');
 
-var config = {
-        apiKey: "AIzaSyAKJ4shHKjGJJKSAYyeNLJLdy2UhxoHM4g",
-        authDomain: "tripsaas-test.firebaseapp.com",
-        databaseURL: "https://tripsaas-test.firebaseio.com",
-        projectId: "tripsaas-test",
-        storageBucket: "tripsaas-test.appspot.com",
-        messagingSenderId: "612781458571"
-      };
+function getFirebaseConfig() {
+    return $.getJSON('/firebaseConfig.json').then(function(data){
+        return data;
+    });
+}
 
-firebase.initializeApp(config);
+getFirebaseConfig().then(function(config) {
+  // var config = {
+  //         apiKey: "AIzaSyAKJ4shHKjGJJKSAYyeNLJLdy2UhxoHM4g",
+  //         authDomain: "tripsaas-test.firebaseapp.com",
+  //         databaseURL: "https://tripsaas-test.firebaseio.com",
+  //         projectId: "tripsaas-test",
+  //         storageBucket: "tripsaas-test.appspot.com",
+  //         messagingSenderId: "612781458571"
+  //       };
+  
 
-const messaging = firebase.messaging();
+  firebase.initializeApp(config);
 
-messaging.setBackgroundMessageHandler(function(payload) {
-  const title = payload.data.title;
-  const options = {
-    click_action: payload.data.click_action,
-    icon: payload.data.icon,
-    body: payload.data.body,
-    data: payload.data
-  };
+  const messaging = firebase.messaging();
 
-  return self.registration.showNotification(title, options);
-});
+  messaging.setBackgroundMessageHandler(function(payload) {
+    const title = payload.data.title;
+    const options = {
+      click_action: payload.data.click_action,
+      icon: payload.data.icon,
+      body: payload.data.body,
+      data: payload.data
+    };
 
-self.addEventListener('notificationclick', function(event) {
-  const pageUrl = event.notification.data.click_action;
-
-  const urlToOpen = new URL(pageUrl, self.location.origin).href;
-
-  const promiseChain = clients.matchAll({
-    type: 'window',
-    includeUncontrolled: true
-  })
-  .then((windowClients) => {
-    let matchingClient = null;
-
-    for (let i = 0; i < windowClients.length; i++) {
-      const windowClient = windowClients[i];
-      if (windowClient.url === urlToOpen) {
-        matchingClient = windowClient;
-        break;
-      }
-    }
-
-    if (matchingClient) {
-      return matchingClient.focus();
-    } else {
-      return clients.openWindow(urlToOpen);
-    }
+    return self.registration.showNotification(title, options);
   });
 
-  event.waitUntil(promiseChain);
+  self.addEventListener('notificationclick', function(event) {
+    const pageUrl = event.notification.data.click_action;
+
+    const urlToOpen = new URL(pageUrl, self.location.origin).href;
+
+    const promiseChain = clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    })
+    .then((windowClients) => {
+      let matchingClient = null;
+
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        if (windowClient.url === urlToOpen) {
+          matchingClient = windowClient;
+          break;
+        }
+      }
+
+      if (matchingClient) {
+        return matchingClient.focus();
+      } else {
+        return clients.openWindow(urlToOpen);
+      }
+    });
+
+    event.waitUntil(promiseChain);
+  });
+
+
 });
 
 
